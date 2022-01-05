3e85b99ceef51f3c9463500b46395b985ef352c1
moyu <1834960035@qq.com>
2022-01-05 11:31:12 +0800

    moyu first1 commit

--- src/main/com/moyu/controller/AController.java
+++ src/main/com/moyu/controller/AController.java
@@ -3,5 +3,6 @@ package com.moyu.controller;
 public class AController {
     public static void main(String[] args) {
         System.out.println("hhhhhhhhhhh");
+        System.out.println("bbbbbbbbbbb");
     }
 }
